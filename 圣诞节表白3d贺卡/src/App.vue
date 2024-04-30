03d639008a7d3d4227804174d6988a9258cdae43
qingshuang.ye <yeqingshuan@tcsc.com>
2024-04-30 17:44:55 +0800

    31-天空与水面

--- 圣诞节表白3d贺卡/src/App.vue
+++ 圣诞节表白3d贺卡/src/App.vue
@@ -7,6 +7,8 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
+import { Water } from "three/examples/jsm/objects/Water2.js";
 import gasp from "gsap";
 
 // 初始化场景
@@ -27,6 +29,10 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+// 设置色调映射
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 0.5;
 
 // 初始化控制器
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -38,12 +44,38 @@ dracoLoader.setDecoderPath("./draco/");
 const gltfLoader = new GLTFLoader();
 gltfLoader.setDRACOLoader(dracoLoader);
 
+// 加载环境纹理
+let rgbeLoader = new RGBELoader();
+rgbeLoader.load("./textures/sky.hdr", (texture) => {
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.background = texture;
+  scene.environment = texture;
+});
+
 // 加载模型
 gltfLoader.load("./model/scene.glb", (gltf) => {
   const model = gltf.scene;
+  model.traverse((child) => {
+    if (child.name === "Plane") {
+      child.visible = false;
+    }
+  });
   model.scale.set(0.1, 0.1, 0.1);
   scene.add(model);
 });
+
+// 创建水面
+const waterGeometry = new THREE.CircleGeometry(300, 32);
+const water = new Water(waterGeometry, {
+  textureWidth: 1024,
+  textureHeight: 1024,
+  color: 0xeeeeff,
+  flowDirection: new THREE.Vector2(1, 1),
+  scale: 100,
+});
+water.rotation.x = -Math.PI / 2;
+water.position.y = -0.4;
+scene.add(water);
 
 // 添加平行光
 const light = new THREE.DirectionalLight(0xffffff, 1);
